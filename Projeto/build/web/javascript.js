6d5b5da8b080f787d32e07eebed9dcc3c7fc6ed9
Rafael Borduchi Amoroso <rafa_borduchi@hotmail.com>
2017-12-03 23:53:51 -0200

    Correção

--- Projeto/build/web/javascript.js
+++ Projeto/build/web/javascript.js
@@ -1,9 +1,9 @@
 function notUser() {
-        $.ajax({
-            type: "get",
-            url: "./UsuarioLogado?id=" + $("#user_id").val(),
-            dataType: "json",
-            success: function (data) {
-                
-            }
+    $.ajax({
+        type: "get",
+        url: "./UsuarioLogado?id=" + $("#user_id").val(),
+        dataType: "json",
+        success: function (data) {                
+        }
+    });
 }
